83ccbd325997e2422ff2ace832cd48ae28b921a0
Jailson Prado <jailsondpd@gmail.com>
2022-02-12 15:34:39 -0300

    New project using firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from "./firebaseConnection";
+import { useState } from "react";
+
+import "./style.css";
 
 function App() {
+  const [titulo, setTitulo] = useState("");
+  const [autor, setAutor] = useState("");
+  const [post, setPost] = useState([]);
+
+   async function handleAdd(){
+      await firebase.firestore().collection('posts')
+       .add({                                       //Gerando ID automatico
+        titulo: titulo,
+        autor: autor
+        // .doc('12345')
+        // .set({
+        //   titulo: titulo,
+        //   autor: autor
+        })
+          .then(() => {
+            alert('Dados salvos com sucesso!')
+            setTitulo('')
+            setAutor('')
+          })
+          .catch((error) => {
+            alert(`erro ao salvar dados ${error}`)
+          })
+  };
+
+  async function buscaPost(){
+   await firebase.firestore().collection('posts')
+      .get()
+        .then((snapshot)=> {
+          let list = [];
+         
+          snapshot.forEach((doc) => {
+            list.push({
+              id: doc.id,
+              titulo: doc.data().titulo,
+              autor: doc.data().autor
+            })
+          })
+
+          setPost(list);
+        })
+          .catch((error) => {
+            alert(`erro ao buscar dados ${error}`)
+          })
+   
+   
+   
+   
+   
+    // await firebase.firestore().collection('posts')
+    //   .doc('12345')
+    //   .get()
+    //   .then((snapshot) => {
+    //     setTitulo(snapshot.data().titulo);
+    //     setAutor(snapshot.data().autor);
+    //   })
+    //   .catch((error) => {
+    //     alert(`Erro ao buscar item ${error}`)
+    //   })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ReactJS + Firebase</h1> <br />
+      <div className="container">
+        <label>Titulo: </label>
+        <textarea
+          type="text"
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+        />
+
+        <label>Autor: </label>
+        <input
+          type="text"
+          value={autor}
+          onChange={(e) => setAutor(e.target.value)}
+        />
+
+        <button onClick={handleAdd}>Cadastrar</button>
+        <button onClick={buscaPost} >Buscar Post</button><br/>
+
+        
+        <ul>
+            {post.map((item) => {
+              return(
+                <li key={item.id}>
+                  <span>Titulo: {item.titulo}</span><br/>
+                  <span>Autor: {item.autor}</span><br/> <br/>
+                </li>
+              )
+            })}
+       
+        </ul>
+     
+      </div>
     </div>
   );
 }
---
+++ src/firebaseConnection.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDCgHwYbFeHTXP_5AVk7mr1Rr4BkJovkeo",
+    authDomain: "curso-8c446.firebaseapp.com",
+    projectId: "curso-8c446",
+    storageBucket: "curso-8c446.appspot.com",
+    messagingSenderId: "252147762637",
+    appId: "1:252147762637:web:e32fc1bbe19d7277653084",
+    measurementId: "G-77LXQSPM49"
+  };
+
+if(!firebase.apps.length) {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
