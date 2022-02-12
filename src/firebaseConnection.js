c2b77c27deea0b069b136acc1c635082f20c7015
Jailson Prado <jailsondpd@gmail.com>
2022-02-12 16:24:27 -0300

    remove itens not use

--- src/firebaseConnection.js
+++ src/firebaseConnection.js
@@ -3,19 +3,18 @@ import firebase from "firebase/app";
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDCgHwYbFeHTXP_5AVk7mr1Rr4BkJovkeo",
+    apiKey: "",
     authDomain: "curso-8c446.firebaseapp.com",
     projectId: "curso-8c446",
     storageBucket: "curso-8c446.appspot.com",
-    messagingSenderId: "252147762637",
-    appId: "1:252147762637:web:e32fc1bbe19d7277653084",
-    measurementId: "G-77LXQSPM49"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 if(!firebase.apps.length) {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  alert("Initialization")
 }
 
 export default firebase;
