de61cb94d92cc4adc7665e05d836486e2adb20dd
Jailson Prado <jailsondpd@gmail.com>
2022-02-12 16:10:28 -0300

    Create method for update itens

--- src/App.js
+++ src/App.js
@@ -4,74 +4,77 @@ import { useState, useEffect } from "react";
 import "./style.css";
 
 function App() {
+  const [idPost, setIdPost] = useState("");
   const [titulo, setTitulo] = useState("");
   const [autor, setAutor] = useState("");
   const [post, setPost] = useState([]);
 
   useEffect(() => {
     async function loadPost(post) {
-      await firebase.firestore().collection('posts')
-      .onSnapshot((doc) => {
-        let myPosts = [];
+      await firebase
+        .firestore()
+        .collection("posts")
+        .onSnapshot((doc) => {
+          let myPosts = [];
 
-        doc.forEach((item) => {
-          myPosts.push({
-            id: item.id,
-            titulo: item.data().titulo,
-            autor: item.data().autor
-          })
-        })
-        setPost(myPosts)
-      })
+          doc.forEach((item) => {
+            myPosts.push({
+              id: item.id,
+              titulo: item.data().titulo,
+              autor: item.data().autor,
+            });
+          });
+          setPost(myPosts);
+        });
     }
 
-    loadPost()
-  },[])
+    loadPost();
+  }, []);
 
-   async function handleAdd(){
-      await firebase.firestore().collection('posts')
-       .add({                                       //Gerando ID automatico
+  async function handleAdd() {
+    await firebase
+      .firestore()
+      .collection("posts")
+      .add({
+        //Gerando ID automatico
         titulo: titulo,
-        autor: autor
+        autor: autor,
         // .doc('12345')
         // .set({
         //   titulo: titulo,
         //   autor: autor
-        })
-          .then(() => {
-            alert('Dados salvos com sucesso!')
-            setTitulo('')
-            setAutor('')
-          })
-          .catch((error) => {
-            alert(`erro ao salvar dados ${error}`)
-          })
-  };
+      })
+      .then(() => {
+        alert("Dados salvos com sucesso!");
+        setTitulo("");
+        setAutor("");
+      })
+      .catch((error) => {
+        alert(`erro ao salvar dados ${error}`);
+      });
+  }
 
-  async function buscaPost(){
-   await firebase.firestore().collection('posts')
+  async function buscaPost() {
+    await firebase
+      .firestore()
+      .collection("posts")
       .get()
-        .then((snapshot)=> {
-          let list = [];
-         
-          snapshot.forEach((doc) => {
-            list.push({
-              id: doc.id,
-              titulo: doc.data().titulo,
-              autor: doc.data().autor
-            })
-          })
+      .then((snapshot) => {
+        let list = [];
 
-          setPost(list);
-        })
-          .catch((error) => {
-            alert(`erro ao buscar dados ${error}`)
-          })
-   
-   
-   
-   
-   
+        snapshot.forEach((doc) => {
+          list.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          });
+        });
+
+        setPost(list);
+      })
+      .catch((error) => {
+        alert(`erro ao buscar dados ${error}`);
+      });
     // await firebase.firestore().collection('posts')
     //   .doc('12345')
     //   .get()
@@ -84,10 +87,35 @@ function App() {
     //   })
   }
 
+  async function editarPost() {
+    await firebase
+      .firestore()
+      .collection("posts")
+      .doc(idPost)
+      .update({
+        titulo: titulo,
+        autor: autor,
+      })
+      .then(() => {
+        alert("Dados editados com sucesso!");
+        setTitulo("");
+        setAutor("");
+        setIdPost("");
+      })
+      .catch((error) => alert(`Erro ao editar dados ${error}`));
+  }
+
   return (
     <div>
       <h1>ReactJS + Firebase</h1> <br />
       <div className="container">
+        <label>ID: </label>
+        <input
+          type="text"
+          value={idPost}
+          onChange={(e) => setIdPost(e.target.value)}
+        />
+
         <label>Titulo: </label>
         <textarea
           type="text"
@@ -103,21 +131,24 @@ function App() {
         />
 
         <button onClick={handleAdd}>Cadastrar</button>
-        <button onClick={buscaPost}>Buscar Post</button><br/>
+        <button onClick={buscaPost}>Buscar Post</button>
+        <br />
+        <button onClick={editarPost}>Editar Post</button>
+        <br />
 
-        
         <ul>
-            {post.map((item) => {
-              return(
-                <li key={item.id}>
-                  <span>Titulo: {item.titulo}</span><br/>
-                  <span>Autor: {item.autor}</span><br/> <br/>
-                </li>
-              )
-            })}
-       
+          {post.map((item) => {
+            return (
+              <li key={item.id}>
+                <span>ID - {item.id}</span> <br />
+                <span>Titulo: {item.titulo}</span>
+                <br />
+                <span>Autor: {item.autor}</span>
+                <br /> <br />
+              </li>
+            );
+          })}
         </ul>
-     
       </div>
     </div>
   );
